ea784cf5356b3155a44b6fcd52da97c94087bc09
resulsilay <resulsilay@gmail.com>
2026-04-16 14:06:36 +0300

    Update: Severity Calculation

--- sentinel-core/src/commonMain/kotlin/sentinel/core/ext/StringExt.kt
+++ sentinel-core/src/commonMain/kotlin/sentinel/core/ext/StringExt.kt
@@ -1,3 +1,9 @@
 package sentinel.core.ext
 
 expect fun String?.toByteList(): List<Byte>
+
+internal fun Any.categoryOf(): String? =
+    this::class.qualifiedName
+        ?.split(".")
+        ?.takeLast(2)
+        ?.firstOrNull()
